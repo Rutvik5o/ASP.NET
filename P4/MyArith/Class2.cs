333d622096e9de9eb5462ab8a81f8abfcb6f50a4
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-07-23 19:42:46 +0530

    University:Ch:1-(4)

---
+++ P4/MyArith/Class2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyArith
+{
+    class multidiv
+    {
+        public int mul(int num1, int num2)
+        {
+            return (num1 * num2);
+        }
+
+        public int div(int num1, int num2)
+        {
+            return (num1 / num2);
+        }
+
+      
+
+    }
+}
