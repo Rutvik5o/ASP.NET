3f6476865617ad1332b627d80d6c881cb3997ace
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-08-25 20:37:01 +0530

    University:Ch:2-(4)

---
+++ P24/Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        int count = 1;
+
+        if(Request.Cookies["myCookies"]!=null)
+        {
+            count = Convert.ToInt16(Request.Cookies["myCookies"].Value);
+            count++;
+        }
+
+        HttpCookie Obj = new HttpCookie("myCookies");
+
+        Obj.Value = Convert.ToString(count);
+
+        Response.Cookies.Add(Obj);
+
+        Label1.Text = "<b> This page is visited " + Convert.ToString(count)+" times on computer<br>";
+
+
+
+
+
+
+
+
+
+
+            
+
+
+    }
+}
