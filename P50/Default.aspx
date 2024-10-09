80d871b3d4c40aa978b610f1112a6e7123ce153b
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-10-09 18:20:09 +0530

    University:Ch:4-(10)

---
+++ P50/Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Untitled Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    <asp:GridView ID="GridView1" runat="server">
+    </asp:GridView>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+    <asp:FormView ID="FormView1" runat="server">
+    <ItemTemplate>
+    
+     <table border=1>
+     <tr>
+     <td>Customer Name</td>
+     <td><%#Eval("cname") %></td>
+     </tr>
+     <tr>
+     <td>Product Name</td>
+     <td><%#Eval("pname") %></td>
+     </tr>
+     <tr>
+     <td>Line Total</td>
+     <td><%#Eval("linetotal") %></td>
+     </tr>
+     </table>
+    </ItemTemplate>
+    </asp:FormView>
+    </form>
+</body>
+</html>
---
+++ P50/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data;
+using System.Data.OleDb;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string s = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=E:\ASP.NET\P502\main.mdb";
+
+        OleDbConnection cn = new OleDbConnection(s);
+
+        string qry = "select c.cname,p.pname,sd.UnitPrice*sd.qty as linetotal from cust c,product p,salesmaster sm,saledetails sd where sm.inumber = sd.inumber and sd.pcode =p.pcode and sm.ccode = c.ccode";
+
+        OleDbCommand cmd = new OleDbCommand(qry, cn);
+
+        OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+
+        DataSet ds = new DataSet();
+
+        da.Fill(ds);
+
+        FormView1.DataSource = ds.Tables[0];
+        FormView1.DataBind();
+
+        GridView1.DataSource = ds.Tables[0];
+        GridView1.DataBind();
+
+        FormView1.AllowPaging = true;
+    }
+}
